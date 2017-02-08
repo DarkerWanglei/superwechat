df2571e46261e5147c276d9322ab8b6c996e24aa
wanglei <912436316@qq.com>
2017-02-08 10:53:36 +0800

    添加欢迎界面

---
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -0,0 +1,53 @@
+package cn.ucai.superwechat.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.LoginActivity;
+import cn.ucai.superwechat.ui.RegisterActivity;
+
+
+/**
+ * Created by Administrator on 2017/1/10.
+ */
+
+/**
+ * 开机
+ */
+public class MFGT {
+    public static void startActivity(Activity activity, Class<?> clz) {
+        activity.startActivity(new Intent(activity, clz));
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    /**
+     * 结束activity
+     *
+     * @param activity
+     */
+    public static void finishActivity(Activity activity) {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    /**
+     * 启动另一个activity
+     *
+     * @param activity
+     * @param intent
+     */
+    public static void startActivity(Activity activity, Intent intent) {
+        activity.startActivity(intent);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void gotoLogin(Activity activity) {
+        startActivity(activity, LoginActivity.class);
+    }
+
+    public static void gotoRegister(Activity activity) {
+        startActivity(activity, RegisterActivity.class);
+    }
+}
