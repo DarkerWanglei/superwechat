58c32ca5a7fa4c025aa22bf67b8b96c7950394d8
wanglei <912436316@qq.com>
2017-02-08 09:40:58 +0800

    修改闪屏以及修改文件名Demo为SuperWeChat

--- superwechat/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ superwechat/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -28,7 +28,7 @@ import com.hyphenate.chat.EMMessage.Type;
 import com.hyphenate.chat.EMOptions;
 import com.hyphenate.chat.EMTextMessageBody;
 
-import cn.ucai.superwechat.db.DemoDBManager;
+import cn.ucai.superwechat.db.SuperWeChatDBManager;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.db.UserDao;
 import cn.ucai.superwechat.domain.EmojiconExampleGroupData;
@@ -63,7 +63,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public class DemoHelper {
+public class SuperWeChatHelper {
     /**
      * data sync listener
      */
@@ -76,7 +76,7 @@ public class DemoHelper {
         void onSyncComplete(boolean success);
     }
 
-    protected static final String TAG = "DemoHelper";
+    protected static final String TAG = "SuperWeChatHelper";
 
     private EaseUI easeUI;
 
@@ -91,9 +91,9 @@ public class DemoHelper {
 
     private UserProfileManager userProManager;
 
-    private static DemoHelper instance = null;
+    private static SuperWeChatHelper instance = null;
 
-    private DemoModel demoModel = null;
+    private SuperWeChatModel demoModel = null;
 
     /**
      * sync groups status listener
@@ -131,12 +131,12 @@ public class DemoHelper {
 
     private boolean isGroupAndContactListenerRegisted;
 
-    private DemoHelper() {
+    private SuperWeChatHelper() {
     }
 
-    public synchronized static DemoHelper getInstance() {
+    public synchronized static SuperWeChatHelper getInstance() {
         if (instance == null) {
-            instance = new DemoHelper();
+            instance = new SuperWeChatHelper();
         }
         return instance;
     }
@@ -147,7 +147,7 @@ public class DemoHelper {
      * @param context application context
      */
     public void init(Context context) {
-        demoModel = new DemoModel(context);
+        demoModel = new SuperWeChatModel(context);
         EMOptions options = initChatOptions();
         //use default options if options is null
         if (EaseUI.getInstance().init(context, options)) {
@@ -666,7 +666,7 @@ public class DemoHelper {
 
         @Override
         public void onContactDeleted(String username) {
-            Map<String, EaseUser> localUsers = DemoHelper.getInstance().getContactList();
+            Map<String, EaseUser> localUsers = SuperWeChatHelper.getInstance().getContactList();
             localUsers.remove(username);
             userDao.deleteContact(username);
             inviteMessgeDao.deleteMessage(username);
@@ -887,8 +887,8 @@ public class DemoHelper {
         return easeUI.getNotifier();
     }
 
-    public DemoModel getModel() {
-        return (DemoModel) demoModel;
+    public SuperWeChatModel getModel() {
+        return (SuperWeChatModel) demoModel;
     }
 
     /**
@@ -1280,7 +1280,7 @@ public class DemoHelper {
         setContactList(null);
         setRobotList(null);
         getUserProfileManager().reset();
-        DemoDBManager.getInstance().closeDB();
+        SuperWeChatDBManager.getInstance().closeDB();
     }
 
     public void pushActivity(Activity activity) {
