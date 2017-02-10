30579a84f3219ef82d28ede7dc0f67a8a4e6f3a5
wanglei <912436316@qq.com>
2017-02-10 19:58:03 +0800

    个人信息页面的编写

--- superwechat/src/cn/ucai/superwechat/ui/ProfileFragment.java
+++ superwechat/src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -68,6 +68,7 @@ public class ProfileFragment extends Fragment {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.layout_profile_view:
+                MFGT.gotoUserProfile(getActivity());
                 break;
             case R.id.txt_money:
                 RedPacketUtil.startChangeActivity(getActivity());
