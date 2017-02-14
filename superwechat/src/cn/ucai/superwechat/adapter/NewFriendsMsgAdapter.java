ccf643490f887681e82a4edbe59ec8873ca2f071
wanglei <912436316@qq.com>
2017-02-14 20:50:06 +0800

    新的朋友页面点击进入详细资料页面

--- superwechat/src/cn/ucai/superwechat/adapter/NewFriendsMsgAdapter.java
+++ superwechat/src/cn/ucai/superwechat/adapter/NewFriendsMsgAdapter.java
@@ -21,6 +21,7 @@ import com.hyphenate.easeui.utils.EaseUserUtils;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.domain.InviteMessage;
+import cn.ucai.superwechat.utils.MFGT;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -54,6 +55,7 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
         if (convertView == null) {
             holder = new ViewHolder();
             convertView = View.inflate(context, R.layout.em_row_invite_msg, null);
+            holder.layoutInvite = (LinearLayout) convertView.findViewById(R.id.layoutInvite);
             holder.avator = (ImageView) convertView.findViewById(R.id.avatar);
             holder.reason = (TextView) convertView.findViewById(R.id.message);
             holder.name = (TextView) convertView.findViewById(R.id.name);
@@ -164,6 +166,12 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
                 holder.status.setBackgroundDrawable(null);
                 holder.status.setEnabled(false);
             }
+            holder.layoutInvite.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MFGT.gotoFriend((Activity) context, msg.getFrom());
+                }
+            });
         }
 
         return convertView;
@@ -282,6 +290,7 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
     }
 
     private static class ViewHolder {
+        LinearLayout layoutInvite;
         ImageView avator;
         TextView name;
         TextView reason;
