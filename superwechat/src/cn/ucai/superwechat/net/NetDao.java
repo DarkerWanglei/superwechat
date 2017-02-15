0bded274139553cccde54c10a7efe81ea31b7b44
wanglei <912436316@qq.com>
2017-02-15 17:45:36 +0800

    初始化联系人数据

--- superwechat/src/cn/ucai/superwechat/net/NetDao.java
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -80,4 +80,12 @@ public class NetDao {
                 .execute(listener);
     }
 
+    public static void loadContacts(Context context, String username, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_DOWNLOAD_CONTACT_ALL_LIST)
+                .addParam(I.Contact.USER_NAME, username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
 }
